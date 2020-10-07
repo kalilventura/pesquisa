77f314427356d7d97c2eebd90dd0236c4b5bd556
Kalil Teixeira Ventura Monteiro <kalilventur@hotmail.com>
2020-10-06 23:28:38 -0300

    Criacao dos charts

--- front-web/src/pages/records/index.tsx
+++ front-web/src/pages/records/index.tsx
@@ -4,7 +4,7 @@ import axios from "axios";
 import { RecordsResponse } from "./types";
 import { formatDate } from "./helpers";
 import Pagination from "./pagination";
-import { Link } from "react-router-dom";
+import Filters from "../filters";
 
 const BASE_URL = "http://localhost:8080";
 
@@ -25,11 +25,7 @@ const Records = () => {
 
   return (
     <div className="page-container">
-      <div className="filters-container records-actions">
-        <Link to="/charts">
-          <button className="action-filters">Ver gráfico</button>
-        </Link>
-      </div>
+      <Filters link="/charts" linkText="VER GRÁFICO" />
       <table className="records-table" cellPadding="0" cellSpacing="0">
         <thead>
           <tr>
